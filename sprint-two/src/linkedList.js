27c65e239be9cc3683b7b553a94af8a3bf01e7b4
David Lee <david7lee@gmail.com>
2015-10-09 21:54:45 -0400

    Implemented linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,41 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var nextNode = new Node();
+    nextNode.value = value;
+    if (list.head === null) {
+      list.head = nextNode;
+      list.tail = nextNode;
+    } else if (list.head) {
+      list.tail.next = nextNode;
+      list.tail = nextNode;
+    }
   };
 
   list.removeHead = function(){
+    var currentHead = list.head.value;
+    var nextItem = list.head.next;
+    list.head = nextItem;
+    return currentHead;
   };
 
-  list.contains = function(target){
+  list.contains = function(target, currentNode){
+    //iterate through the linked list starting at the value of the node at list.head, check for the target
+    //next, check next property of the node at list .head, and check that value for the target
+
+      currentNode =  currentNode || list.head;
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        if (currentNode.next === null) {
+          return false;
+        } else {
+          currentNode = currentNode.next;
+          return list.contains(target, currentNode);
+        }
+      }
+
+
   };
 
   return list;
