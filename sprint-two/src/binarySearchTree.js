20b49b6d030a67a851ce6420e023c43b2713fe1b
David Lee <david7lee@gmail.com>
2015-10-11 18:09:50 -0400

    Implemented binary search tree; modified spec, changed to psuedoclassical pattern

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,53 @@
 var BinarySearchTree = function(value){
+  this.left = {};
+  this.right = {};
+  this.value = value;
 };
 
+BinarySearchTree.prototype.insert = function(value){
+  // original code
+ 
+  if( value < this.value ){
+    if ( this.left.value ){
+      this.left.insert(value, this.left);
+    } else {
+      this.left = new BinarySearchTree(value);
+      return this.left;
+    }
+  } else {
+    if ( this.right.value ){
+      this.right.insert(value, this.right);
+    } else {
+      this.right = new BinarySearchTree(value);
+      return this.right;
+    }
+  }
+  return value;
+  
+};
+
+BinarySearchTree.prototype.contains = function(value){
+  // check value against this.value
+  if (value === this.value){
+    return true;
+  } else if ( value < this.value && this.left.value ) {
+    return this.left.contains(value);
+  } else if ( value > this.value && this.right.value ){
+    return this.right.contains(value);
+  }
+  return false;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb){
+  cb(this.value);
+  if (this.left.value){
+    return this.left.depthFirstLog(cb);
+  }
+  if (this.right.value){
+    return this.right.depthFirstLog(cb);
+  }
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
